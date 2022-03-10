338d9e54b77fbf4bd69bacd3e0b0d941dc6d1518
francocortina <franco.cug@gmail.com>
2022-03-10 03:42:46 -0300

    alcance de variables

--- src/EJ5variables/Variables.java
+++ src/EJ5variables/Variables.java
@@ -4,8 +4,23 @@ public class Variables {
     public static void main(String[] args) {
         int variable1;
         int variable2,variable3;
+        //entero
         int variable4 = 23;
 
+        //decimal
+        float variable5=5.6f;
+        //decimal y precision doble
+        double cariable6=5.6;
+
+        char a='a';
+        //booleanos
+        boolean falseotrue= false;
+
+        //para poner las comillas se una esta barra \
+        char comillaSimple= '\'';
+        System.out.println("Se imprime la comilla : "+ comillaSimple);
+        String stringliteral= "el comun";
+        String stringliteral2= null;
 
     }
 }
